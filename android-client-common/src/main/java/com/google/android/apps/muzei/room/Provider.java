d34c64648148c2db67b102f88d3508848b610557
Ian Lake <ian.hannibal.lake@gmail.com>
2017-09-09 17:26:19 -0700

    Load new artwork, fallback to random, non-recent artwork
    Keep track of the maxLoadedArtworkId and recentArtworkIds
    by Provider to:
    - Try to load new artwork (_id > maxLoadedArtworkIds) or
    - Load random artwork not seen recently

--- android-client-common/src/main/java/com/google/android/apps/muzei/room/Provider.java
+++ android-client-common/src/main/java/com/google/android/apps/muzei/room/Provider.java
@@ -23,6 +23,9 @@ import android.content.ComponentName;
 import android.support.annotation.NonNull;
 
 import com.google.android.apps.muzei.room.converter.ComponentNameTypeConverter;
+import com.google.android.apps.muzei.room.converter.ArrayDequeLongTypeConverter;
+
+import java.util.ArrayDeque;
 
 /**
  * Provider information's representation in Room
@@ -32,6 +35,12 @@ public class Provider {
     @TypeConverters({ComponentNameTypeConverter.class})
     @PrimaryKey
     public ComponentName componentName;
+
+    public long maxLoadedArtworkId;
+
+    @TypeConverters({ArrayDequeLongTypeConverter.class})
+    @NonNull
+    public ArrayDeque<Long> recentArtworkIds = new ArrayDeque<>();
 
     public Provider(@NonNull ComponentName componentName) {
         this.componentName = componentName;
